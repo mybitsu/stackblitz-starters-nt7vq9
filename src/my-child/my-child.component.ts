3c09575f4054cdb2de542cfd2faa329d479d412f
mymitsu <135013543+mymitsu@users.noreply.github.com>
2023-05-30 11:39:39 +0200

    Move main HTML out in file.

---
+++ src/my-child/my-child.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-child',
+  templateUrl: './my-child.component.html',
+  styleUrls: ['./my-child.component.css']
+})
+export class MyChildComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/my-parent/my-parent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-parent',
+  templateUrl: './my-parent.component.html',
+  styleUrls: ['./my-parent.component.css']
+})
+export class MyParentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
